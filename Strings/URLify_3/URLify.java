bc4f23dc3349110b61362e193d80398d1bf67aea
Anastasia Kirillova <anki0216@netcracker.com>
2018-06-13 16:29:48 +0300

    added space counting

--- Strings/URLify_3/URLify.java
+++ Strings/URLify_3/URLify.java
@@ -2,17 +2,28 @@ import java.util.Scanner;
 
 // runtime: O(N)
 public class URLify {
+
+    private static int countSpaces(char[] s, int length) {
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (s[i] == ' ') {
+                count++;
+            }
+        }
+        return length + count * 2 - 1;
+    }
+
     private static char[] replaceSpaces(char[] s, int length) {
-        int len = s.length - 1;
+        int count = countSpaces(s, length);
         for (int i = length - 1; i >= 0; i--) {
             if (s[i] == ' ') {
-                s[len] = '0';
-                s[len - 1] = '2';
-                s[len - 2] = '%';
-                len = len - 3;
+                s[count] = '0';
+                s[count - 1] = '2';
+                s[count - 2] = '%';
+                count = count - 3;
             } else {
-                s[len] = s[i];
-                len--;
+                s[count] = s[i];
+                count--;
             }
         }
         return s;
